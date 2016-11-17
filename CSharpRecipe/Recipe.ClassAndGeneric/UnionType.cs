b7406ed6022dd96ec74a9a106da05715886cc601
caronyan <carony@163.com>
2016-11-17 15:53:38 +0800

    Add union type

---
+++ CSharpRecipe/Recipe.ClassAndGeneric/UnionType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Recipe.ClassAndGeneric
+{
+    /// <summary>
+    /// Using StructLayout and FieldOffset to implement a union type
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    struct SignedNumberWithText
+    {
+        [FieldOffset(0)]
+        public sbyte Num1;
+        [FieldOffset(0)]
+        public short Num2;
+        [FieldOffset(0)]
+        public int Num3;
+        [FieldOffset(0)]
+        public float Num4;
+        [FieldOffset(0)]
+        public long Num5;
+        [FieldOffset(0)]
+        public double Num6;
+        [FieldOffset(16)]
+        public string Text1;
+    }
+}

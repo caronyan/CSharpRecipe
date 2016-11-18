5942c4ab98056faccbb2aef551d3c40beb928736
caronyan <carony@163.com>
2016-11-18 16:54:56 +0800

    Add [using] range control

---
+++ CSharpRecipe/Recipe.ClassAndGeneric/UsingRange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.ClassAndGeneric
+{
+    /// <summary>
+    /// Using [using] to dispose temp object automatically
+    /// </summary>
+    public class UsingRange
+    {
+        public void WriteSomething()
+        {
+            using (FileStream fs = new FileStream("Text.txt", FileMode.Create))
+            {
+                fs.WriteByte((byte)1);
+                fs.WriteByte((byte)2);
+                fs.WriteByte((byte)3);
+
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine("some text");
+                }
+            }
+        }
+    }
+}

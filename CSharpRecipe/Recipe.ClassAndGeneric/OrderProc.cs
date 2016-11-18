30a5f0b28c8be0d8672849279a4213cd7a4f1930
caronyan <carony@163.com>
2016-11-18 21:14:44 +0800

    Add orderby class

---
+++ CSharpRecipe/Recipe.ClassAndGeneric/OrderProc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.ClassAndGeneric
+{
+    public class OrderByTest
+    {
+        public static SortedList<int, string> Data = new SortedList<int, string>()
+        {
+            { 2,"two"},
+            { 5,"five"},
+            { 3,"three"},
+            { 1,"one"},
+        };
+
+        public void SortByLinq()
+        {            
+            //SortedList default
+            Console.WriteLine("SortedList default:\n");
+            foreach (KeyValuePair<int, string> kvp in Data)
+            {                
+                Console.WriteLine($"\t{kvp.Key}\t{kvp.Value}\n");
+            }
+
+            //Linq sorted
+            var query = from d in Data
+                        orderby d.Key descending
+                        select d;
+
+            Console.WriteLine("Linq sorted:\n");
+            foreach (KeyValuePair<int, string> kvp in query)
+            {                
+                Console.WriteLine($"\t{kvp.Key}\t{kvp.Value}\n");
+            }            
+        }
+    }
+}

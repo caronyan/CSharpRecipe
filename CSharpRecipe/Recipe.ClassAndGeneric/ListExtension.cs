9783dc980f86757223a0b32de6338571d81c3670
caronyan <carony@163.com>
2016-11-23 21:26:00 +0800

    Add List<T> extension(search)

---
+++ CSharpRecipe/Recipe.ClassAndGeneric/ListExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.ClassAndGeneric
+{
+    static class ListExtension
+    {
+        public static IEnumerable<T> GetAll<T>(this List<T> myList, T searchValue) => myList.Where(t => t.Equals(searchValue));
+
+        public static T[] BinarySearchGetAll<T>(this List<T> myList, T searchValue)
+        {
+            List<T> retObjs = new List<T>();
+
+            int center = myList.BinarySearch(searchValue);
+            if (center>0)
+            {
+                retObjs.Add(myList[center]);
+
+                int left = center;
+                while (left>0&&myList[left-1].Equals(searchValue))
+                {
+                    left -= 1;
+                    retObjs.Add(myList[center]);
+                }
+
+                int right = center;
+                while (right<(myList.Count-1)&&myList[right+1].Equals(searchValue))
+                {
+                    right += 1;
+                    retObjs.Add(myList[center]);
+                }
+            }
+
+            return retObjs.ToArray();
+        }
+
+        public static int CountAll<T>(this List<T> myList, T searchValue) => myList.GetAll(searchValue).Count();
+
+        public static int BinarySearchCountAll<T>(this List<T> myList, T searchValue)
+            => BinarySearchGetAll(myList, searchValue).Count();
+    }
+}

3eb9415f89ca32db2bf2d3eb3e5d93afb0800adf
caronyan <carony@163.com>
2016-11-26 21:13:52 +0800

    Add where extension

---
+++ CSharpRecipe/Recipe.ClassAndGeneric/ManualExtensionWhere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.ClassAndGeneric
+{
+    public static class ManualExtensionWhere
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentException();
+            }
+
+            return WhereImpl(source, predicate);
+        }
+
+        private static IEnumerable<T> WhereImpl<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}

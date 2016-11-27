fa3003ae9ace5a231e6b95f0098525e6eda8962b
caronyan <carony@163.com>
2016-11-27 20:43:08 +0800

    Add dictionary sorting

---
+++ CSharpRecipe/Recipe.ClassAndGeneric/DictionarySorting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.ClassAndGeneric
+{
+    public class DictionarySorting
+    {
+        public Dictionary<int, string> Hash = new Dictionary<int, string>()
+        {
+            { 2,"two"},
+            { 1,"one"},
+            { 5,"five"},
+            { 4,"four"},
+            { 3,"three"}
+        };
+
+        public void SortingByLinq()
+        {
+            var x = from k in Hash.Keys
+                orderby k ascending
+                select k;
+
+            foreach (var k in x)
+            {
+                Console.WriteLine($"key:{k},Value:{Hash[k]}");
+            }
+        }
+    }
+}

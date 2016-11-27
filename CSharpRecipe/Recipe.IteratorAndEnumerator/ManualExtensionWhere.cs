65dec3bbaebe1a9e67799fe3410dd35587d88334
caronyan <carony@163.com>
2016-11-27 22:20:14 +0800

    Proj modify

--- CSharpRecipe/Recipe.IteratorAndEnumerator/ManualExtensionWhere.cs
+++ CSharpRecipe/Recipe.IteratorAndEnumerator/ManualExtensionWhere.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Recipe.ClassAndGeneric
+namespace Recipe.IteratorAndEnumerator
 {
     public static class ManualExtensionWhere
     {

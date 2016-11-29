0b07ae82f509a06485eeb88dc204708295b95b5f
caronyan <carony@163.com>
2016-11-29 19:40:10 +0800

    Add base64 encoder/decoder

---
+++ CSharpRecipe/Recipe.DataType/DataTypeExtMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.DataType
+{
+    static class DataTypeExtMethods
+    {
+        public static string Base64EncodeBytes(this byte[] inputBytes) => (Convert.ToBase64String(inputBytes));
+
+        public static byte[] Base64DecodeString(this string inputStr) => (Convert.FromBase64String(inputStr));
+    }
+}

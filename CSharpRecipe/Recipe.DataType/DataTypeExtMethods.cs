2ba22ea4d5535547197b576004eb1e5c52f043d7
caronyan <carony@163.com>
2016-11-30 21:35:12 +0800

    Add data narrowing check

--- CSharpRecipe/Recipe.DataType/DataTypeExtMethods.cs
+++ CSharpRecipe/Recipe.DataType/DataTypeExtMethods.cs
@@ -11,5 +11,6 @@ namespace Recipe.DataType
         public static string Base64EncodeBytes(this byte[] inputBytes) => (Convert.ToBase64String(inputBytes));
 
         public static byte[] Base64DecodeString(this string inputStr) => (Convert.FromBase64String(inputStr));
+        public static int AddNarrowingChecked(this long lhs, long rhs) => checked ((int) (lhs + rhs));
     }
 }

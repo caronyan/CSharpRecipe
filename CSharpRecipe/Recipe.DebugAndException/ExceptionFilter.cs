dbb547200a7ab094efc28d11274209e75b68ffb4
caronyan <carony@163.com>
2016-12-05 18:50:41 +0800

    Add exception filter

---
+++ CSharpRecipe/Recipe.DebugAndException/ExceptionFilter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.DebugAndException
+{
+    public class ExceptionFilter
+    {
+        public void ProtectedCallTheDatabase(string problem)
+        {
+            try
+            {
+                CallTheDatabase(problem);
+                Console.WriteLine("********");
+            }
+            catch (DatabaseException dex) when (dex.Number == -2)
+            {
+                Console.WriteLine("********");                
+            }
+        }
+
+        public void CallTheDatabase(string problem)
+        {
+
+        }
+    }
+
+    [Serializable]
+    public class DatabaseException : DbException
+    {
+        public DatabaseException(string message) : base(message)
+        {
+        }
+
+        public byte Class { get; set; }
+        public Guid ClientConnectionId { get; set; }
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public SqlErrorCollection Errors { get; set; }
+        public int LineNumber { get; set; }
+        public int Number { get; set; }
+        public string Procedure { get; set; }
+        public string Server { get; set; }
+        public override string Source => base.Source;
+        public byte State { get; set; }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}

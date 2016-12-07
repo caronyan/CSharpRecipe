f0b5c97a6e03af43d082b8e40ad4c8cb690e3cf9
caronyan <carony@163.com>
2016-12-07 13:32:39 +0800

    Add local variable function

---
+++ CSharpRecipe/Recipe.ReflectionAndDynamic/LocalVariable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.ReflectionAndDynamic
+{
+    public class LocalVariableTest
+    {
+        public static IReadOnlyCollection<LocalVariableInfo> GetLocalVars(string asmPath, string typeName,
+            string methodName)
+        {
+            Assembly asm = Assembly.LoadFrom(asmPath);
+            Type asmType = asm.GetType(typeName);
+
+            MethodInfo mi = asmType.GetMethod(methodName);
+            MethodBody mb = mi.GetMethodBody();
+
+            ReadOnlyCollection<LocalVariableInfo> vars = (ReadOnlyCollection<LocalVariableInfo>) mb.LocalVariables;
+
+            return vars;
+        }
+    }
+}

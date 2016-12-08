7b1d4f382954f40d77fffa5237d89a3f8e1a6191
caronyan <carony@163.com>
2016-12-08 11:45:50 +0800

    Add web request generator

---
+++ CSharpRecipe/Recipe.Web/HttpWebRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Web
+{
+    public class HttpWebRequestTest
+    {
+        public static HttpWebRequest GenerateHttpWebRequest(Uri uri)
+        {
+            HttpWebRequest httpRequest = (HttpWebRequest) WebRequest.Create(uri);
+            return httpRequest;
+        }
+
+        public static HttpWebRequest GenerateHttpWebRequest(Uri uri, string postData, string contentType)
+        {
+            HttpWebRequest httpRequest = GenerateHttpWebRequest(uri);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(postData);
+            httpRequest.ContentType = contentType;
+            httpRequest.ContentLength = postData.Length;
+
+            using (Stream requeStream=httpRequest.GetRequestStream())
+            {
+                requeStream.Write(bytes, 0, bytes.Length);
+            }
+
+            return httpRequest;
+        }
+    }
+}

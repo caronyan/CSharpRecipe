2ea174cbc47f6325c05a839f39dce9cfa83522aa
caronyan <carony@163.com>
2016-12-20 00:20:10 +0800

    Add thread safe event trigger

---
+++ CSharpRecipe/Recipe.ThreadAndConcurrent/EventRaiseExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Recipe.ThreadAndConcurrent
+{
+    public static class EventRaiseExtentions
+    {
+        public static void Raise<TEventArgs>(this TEventArgs e, object sender,
+            ref EventHandler<TEventArgs> eventDelegate)
+        {
+            EventHandler<TEventArgs> temp = Volatile.Read(ref eventDelegate);
+            if (temp != null)
+            {
+                temp(sender, e);
+            }
+        }
+    }
+}

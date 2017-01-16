e67d9e1ef9f1ec8d74df47d69feed16889e0c927
caronyan <carony@163.com>
2017-01-16 23:27:43 +0800

    Add custom tcp client constructor

--- CSharpRecipe/Recipe.Web/TcpClient.cs
+++ CSharpRecipe/Recipe.Web/TcpClient.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Recipe.Web
 {
-    public class TcpClient
+    public class CustomTcpClient : IDisposable
     {
+        private TcpClient _client;
+        private IPEndPoint _endPoint;
+        private bool _disposed;
+
+        #region Properties
+
+        public IPAddress Address { get; }
+        public int Port { get; }
+        public string SSLServerName { get; }
+
+        #endregion
+
+        public CustomTcpClient(IPAddress address, int port, string sslServerName = null)
+        {
+            Address = address;
+            Port = port;
+            _endPoint = new IPEndPoint(Address, Port);
+            SSLServerName = sslServerName;
+        }
 
     }
 }
